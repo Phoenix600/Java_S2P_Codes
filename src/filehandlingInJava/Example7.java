9acf92d156bc0d95d10f7bd0b1042df29756fdec
pranay <pranayramteke613@gmail.com>
2024-03-06 22:28:17 -0800

    Added Code For IOStreams In Java

---
+++ src/filehandlingInJava/Example7.java
@@ -0,0 +1,20 @@
+package filehandlingInJava;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Example7 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:/Users/root/Downloads/Java_S2P_Codes/src/filehandlingInJava/data/data2.ser");
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write("Java May Be Good, But C is Better".getBytes());
+        fos.flush();
+
+        byte b[] = {65,66,67};
+        fos.write(b);
+
+        fos.close();
+
+    }
+}
