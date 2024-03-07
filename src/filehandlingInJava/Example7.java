73f92302089bb43bb6322ed50dd1c47a861cc94d
pranay <pranayramteke613@gmail.com>
2024-03-06 23:10:55 -0800

    Added code Generic Classes

--- src/filehandlingInJava/Example7.java
+++ src/filehandlingInJava/Example7.java
@@ -16,14 +16,5 @@ public class Example7 {
 
     }
 
-    public static class Example8 {
-        public static void main(String[] args) throws IOException, ClassNotFoundException {
-            File file = new File("C:/Users/root/Downloads/Java_S2P_Codes/src/filehandlingInJava/data/data2.ser");
-            FileInputStream fis = new FileInputStream("C:/Users/root/Downloads/Java_S2P_Codes/src/filehandlingInJava/data/data2.ser");
-            ObjectInputStream ois = new ObjectInputStream(fis);
 
-            int data = ois.available();
-            System.out.println(data);
-        }
-    }
 }
--- src/filehandlingInJava/Example8.java
+++ src/filehandlingInJava/Example8.java
@@ -1,9 +1,14 @@
 package filehandlingInJava;
 
-import java.io.FileOutputStream;
+import java.io.*;
 
 public class Example8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File file = new File("C:/Users/root/Downloads/Java_S2P_Codes/src/filehandlingInJava/data/data2.ser");
+        FileInputStream fis = new FileInputStream("C:/Users/root/Downloads/Java_S2P_Codes/src/filehandlingInJava/data/data2.ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
 
+        int data = ois.available();
+        System.out.println(data);
     }
 }
