78e172f431f9696010c6b28deed90c1f16e11391
phoenix600 <pranayramteke613@gmail.com>
2024-03-26 23:04:23 -0700

    Added Code for Stream Intermediate Operations Min

--- src/collectionInJava/streamAPI/Example10.java
+++ src/collectionInJava/streamAPI/Example10.java
@@ -1,10 +1,13 @@
 package collectionInJava.streamAPI;
 
-import java.util.stream.Stream;
+import java.util.*;
 
 public class Example10 {
     public static void main(String[] args)
     {
-    // Adding new codes very soon
+        List<Integer> numbers = Arrays.asList(3, 1, 4, 1, 5, 9, 2);
+        Optional<Integer> minNumber = numbers.stream().min(Integer::compareTo);
+        System.out.println(minNumber.isPresent());
+        System.out.println(minNumber.get());
     }
 }
